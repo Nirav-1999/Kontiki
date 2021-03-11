412b49021f913275c58850d9ff7ef909ab2dd0cc
Nirav <niravjain1709@gmail.com>
2021-03-11 23:31:29 +0530

    asgi

--- fitmove/fitmove/asgi.py
+++ fitmove/fitmove/asgi.py
@@ -13,8 +13,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import video.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitmove.settings')
+
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
